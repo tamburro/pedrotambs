b7e1ae47b6696eaff734a09895adf6450986318c
tamburro <pedropaulotjr@gmail.com>
2026-03-31 00:15:29 -0300

    feat: add company and education logos, adjust resume styling

--- src/components/sections/resume.jsx
+++ src/components/sections/resume.jsx
@@ -17,11 +17,11 @@ const Resume = () => {
                             <div className="single-resume">
                                 <h2>{t.resume.experience}</h2>
                                 <div className="experience-list">
-                                    <Card icon={<RiBriefcaseLine />} year="Nov 2022 – Present" title="Designer Visual Pleno" institution="Editora Globo" description={t.resume.exp1} />
-                                    <Card icon={<RiBriefcaseLine />} year="Jun 2020 – Nov 2021" title="Designer Visual Junior" institution="YDUQS" description={t.resume.exp2} />
-                                    <Card icon={<RiBriefcaseLine />} year="Jul 2019 – Jun 2020" title="Designer Visual PJ" institution="Estácio" description={t.resume.exp3} />
+                                    <Card logo="/images/companies/globo.jpeg" year="Nov 2022 – Present" title="Designer Visual Pleno" institution="Editora Globo" description={t.resume.exp1} />
+                                    <Card logo="/images/companies/yduqs.jpeg" year="Jun 2020 – Nov 2021" title="Designer Visual Junior" institution="YDUQS" description={t.resume.exp2} />
+                                    <Card logo="/images/companies/estacio.jpeg" year="Jul 2019 – Jun 2020" title="Designer Visual PJ" institution="Estácio" description={t.resume.exp3} />
                                     {expanded && (
-                                        <Card icon={<RiBriefcaseLine />} year="Feb 2011 – Jan 2012" title="Design Intern" institution="Conspiração Filmes" description={t.resume.exp4} />
+                                        <Card logo="/images/companies/conspiracao.jpeg" year="Feb 2011 – Jan 2012" title="Design Intern" institution="Conspiração Filmes" description={t.resume.exp4} />
                                     )}
                                 </div>
                                 <div style={{ marginTop: '16px' }}>
@@ -39,9 +39,9 @@ const Resume = () => {
                             <div className="single-resume">
                                 <h2>{t.resume.education}</h2>
                                 <div className="experience-list">
-                                    <Card icon={<RiGraduationCapLine />} year="Jul 2025 – Set 2026" title="MBA em Inovação Orientada à IA e UX" institution="UX Unicórnio" description={t.resume.edu1} />
-                                    <Card icon={<RiGraduationCapLine />} year="Abr 2023 – Abr 2024" title="MBA em UX Design & Arquitetura da Informação" institution="Instituto Infnet – ECDD" description={t.resume.edu2} />
-                                    <Card icon={<RiGraduationCapLine />} year="2013 – 2018" title="Graduação em Design Gráfico" institution="Senac RJ" description={t.resume.edu3} />
+                                    <Card logo="/images/education/uxunicornio.jpeg" year="Jul 2025 – Set 2026" title="MBA em Inovação Orientada à IA e UX" institution="UX Unicórnio" description={t.resume.edu1} />
+                                    <Card logo="/images/education/infnet.jpeg" year="Abr 2023 – Abr 2024" title="MBA em UX Design & Arquitetura da Informação" institution="Instituto Infnet – ECDD" description={t.resume.edu2} />
+                                    <Card logo="/images/education/senac.jpeg" year="2013 – 2018" title="Graduação em Design Gráfico" institution="Senac RJ" description={t.resume.edu3} />
                                 </div>
                             </div>
                         </div>
@@ -54,11 +54,13 @@ const Resume = () => {
 
 export default Resume
 
-const Card = ({ icon, year, title, institution, description }) => {
+const Card = ({ icon, logo, year, title, institution, description }) => {
     return (
         <SlideUp>
             <div className="resume-item">
-                <div className="icon">{icon}</div>
+                <div className="icon">
+                    {logo ? <img src={logo} alt={institution} /> : icon}
+                </div>
                 <div className="content">
                     <span className="years">{year}</span>
                     <h4>{title}</h4>
