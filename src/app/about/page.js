8a46fb7a7c7d38b2be820051c54419f65cf00e2d
tamburro <pedropaulotjr@gmail.com>
2026-03-31 01:42:43 -0300

    fix: remove testimonials from about page and fix build error

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Summery from '@/components/sections/summery'
 import Resume from '@/components/sections/resume'
-import Testimonials from '@/components/sections/testimonials'
+
 
 const About = () => {
     return (
         <>
             <Summery />
             <Resume />
-            <Testimonials />
            
         </>
     )
